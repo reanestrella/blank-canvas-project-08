4265b1796a81d640b263bd20a27b94ab1a481bcc
reanestrella <reanestrella@gmail.com>
2026-02-19 10:39:28 -0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Projeto abriu ✅</h1>
+      <p>Se você está vendo isso, o build está OK.</p>
+    </div>
+  );
+}
