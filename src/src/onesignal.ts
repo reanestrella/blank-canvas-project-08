46c9b879958fd6ccc9690ba82e5d64511b44d642
reanestrella <reanestrella@gmail.com>
2026-04-07 11:35:35 -0300

    Create onesignal.ts

---
+++ src/src/onesignal.ts
@@ -0,0 +1,21 @@
+import OneSignal from 'react-onesignal';
+
+export async function initOneSignal() {
+  await OneSignal.init({
+    appId: "fbb0594c-d29a-4f6a-af12-cb279e18969e",
+
+    allowLocalhostAsSecureOrigin: true,
+
+    serviceWorkerPath: "/OneSignalSDKWorker.js",
+    serviceWorkerUpdaterPath: "/OneSignalSDKUpdaterWorker.js",
+
+    notifyButton: {
+      enable: true,
+    },
+  });
+}
+
+export async function pedirPermissao() {
+  const permission = await OneSignal.Notifications.requestPermission();
+  console.log("Permissão:", permission);
+}
