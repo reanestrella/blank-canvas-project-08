c2d622fe4a4caeaf7bca321579b8384b7da4c374
reanestrella <reanestrella@gmail.com>
2026-04-08 09:08:55 -0300

    Update onesignal.ts

--- src/lib/onesignal.ts
+++ src/lib/onesignal.ts
@@ -1,38 +1,17 @@
-import OneSignal from "react-onesignal";
+import OneSignal from 'react-onesignal';
 
-export async function initOneSignal() {
+export const initOneSignal = async () => {
   await OneSignal.init({
     appId: "fbb0594c-d29a-4f6a-af12-cb279e18969e",
 
-    allowLocalhostAsSecureOrigin: true,
-
-    serviceWorkerPath: "/OneSignalSDKWorker.js",
-    serviceWorkerUpdaterPath: "/OneSignalSDKUpdaterWorker.js",
-
     notifyButton: {
       enable: true,
-      prenotify: true,
-      showCredit: false,
-      text: {
-        "tip.state.unsubscribed": "Inscreva-se para notificações",
-        "tip.state.subscribed": "Você está inscrito",
-        "tip.state.blocked": "Você bloqueou as notificações",
-        "message.prenotify": "Clique para se inscrever",
-        "message.action.subscribed": "Obrigado por se inscrever!",
-        "message.action.resubscribed": "Você está inscrito para notificações",
-        "message.action.subscribing": "Inscrevendo...",
-        "message.action.unsubscribed": "Você não receberá mais notificações",
-        "dialog.main.title": "Gerenciar notificações",
-        "dialog.main.button.subscribe": "INSCREVER",
-        "dialog.main.button.unsubscribe": "CANCELAR INSCRIÇÃO",
-        "dialog.blocked.title": "Desbloquear notificações",
-        "dialog.blocked.message": "Siga estas instruções para permitir notificações:",
-      },
     },
-  });
-}
 
-export async function pedirPermissao() {
-  const permission = await OneSignal.Notifications.requestPermission();
-  console.log("Permissão:", permission);
-}
+    allowLocalhostAsSecureOrigin: true,
+  });
+};
+
+export const pedirPermissao = async () => {
+  await OneSignal.showSlidedownPrompt();
+};
