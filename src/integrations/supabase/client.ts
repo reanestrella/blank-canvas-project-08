5877a28e605d122d09364c4ab4bfbca382a732e2
reanestrella <reanestrella@gmail.com>
2026-04-24 09:46:24 -0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,7 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://ycaiusoyqoeccmmixgrf.supabase.co";
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InljYWl1c295cW9lY2NtbWl4Z3JmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE1NzIzMTgsImV4cCI6MjA4NzE0ODMxOH0.uj4lXNqc88XP8drTvxlwhswxK18sJtRRoVs8JIRTEvI";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL!,
+  import.meta.env.VITE_SUPABASE_ANON_KEY!,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
+    },
+  }
+);
